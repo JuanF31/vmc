7bf26f8c3ec95339feac1fe61907d6ac04803416
unknown <sebashernandez03@hotmail.com>
2022-04-01 12:40:13 -0600

    aviso de privacidad

--- vmcorrea/js/main.js
+++ vmcorrea/js/main.js
@@ -57,4 +57,27 @@ document.querySelectorAll(".mega_link").forEach(s => s.addEventListener("mouseou
 }));
 
 //fin 
+// let sidebar = document.querySelector(".sidebar");
+// let closeBtn = document.querySelector("#btn");
+// let searchBtn = document.querySelector(".bx-search");
 
+// closeBtn.addEventListener("click", ()=>{
+//   sidebar.classList.toggle("open");
+//   menuBtnChange();//calling the function(optional)
+// });
+
+// searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+//   sidebar.classList.toggle("open");
+//   menuBtnChange(); //calling the function(optional)
+// });
+
+// // following are the code to change sidebar button(optional)
+// function menuBtnChange() {
+//  if(sidebar.classList.contains("open")){
+//    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+//  }else {
+//    closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+//  }
+// }
+
+//POLITICA DE PRIVACIDAD
